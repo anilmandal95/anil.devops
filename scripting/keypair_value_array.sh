d280309f34019386caffe6825c5569b57439a4ee
Anil <akmandalgoals@gmail.com>
2023-08-13 22:19:12 +0530

    added new file

---
+++ scripting/keypair_value_array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#key value pair array
+
+declare -A myarray
+
+myarray=( [name]=anil [age]=25 [city]=patna )
+
+echo "name is ${myarray[name]}"
+
+
+echo "Age is ${myarray[age]} and City is ${myarray[city]}"
